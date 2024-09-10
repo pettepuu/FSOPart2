f891970db819567cf1329d6758e6f6bdb2c2f433
pettepuu <113012213+pettepuu@users.noreply.github.com>
2024-09-10 11:54:31 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -13,97 +13,101 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.json());
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => response.json(persons))
+    .catch(error => next(error));
+});
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-  next(error)
-}
-
-app.use(errorHandler)
-
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
-})
-
-app.get('/info', (req, res) => {
+app.get('/info', async (req, res, next) => {
+  try {
+    const count = await Person.countDocuments();
     const currentTime = new Date();
     const responseText = `
-        <p>Phonebook has info for ${Person.countDocuments()} people</p>
-        <p>${currentTime}</p>
+      <p>Phonebook has info for ${count} people</p>
+      <p>${currentTime}</p>
     `;
     res.send(responseText);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
-})
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
 
-
-app.delete('/api/persons/:id', async (request, response) => {
-    try {
-      await Person.findByIdAndDelete(request.params.id);
-      response.status(204).end();
-    } catch (error) {
-      response.status(400).json({ error: 'Malformed ID' });
-    }
-  });
+app.delete('/api/persons/:id', async (request, response, next) => {
+  try {
+    await Person.findByIdAndDelete(request.params.id);
+    response.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body;
 
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-    Person.findByIdAndUpdate(request.params.id, person, { new: true } )
-      .then(updatePerson => {
-        response.json(updatePerson)
-      })
-      .catch(error => next(error))
-  })
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-
-app.post('/api/persons', async (request, response) => {
-    const body = request.body
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({ 
-          error: 'Name or number is missing' 
-        });
-    }
-
-    const person = new Person({
-        id: Math.random()*1500 + 1,
-        name: body.name,
-        number: body.number,
-    });
-
-    try {
-      person.save()
-      .then(savedPerson => {
-        response.json(savedPerson)
-      })
-    } catch (error) {
-        console.error('Error saving person:', error);
-        response.status(500).json({ error: 'Internal Server Error' });
-    }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(error => next(error));
 });
-  
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.post('/api/persons', async (request, response, next) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number is missing',
+    });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  try {
+    const savedPerson = await person.save();
+    response.json(savedPerson);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: 'Validation Error' });
+  } else if (error.name === 'MongoServerError' && error.code === 11000) {
+    return response.status(400).send({ error: 'Duplicate key error' });
+  }
+  
+  response.status(500).send({ error: 'Internal Server Error' });
+};
+
+// Apply error handler middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
