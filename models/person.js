c8d067a45949121ce7d5d1789a5bb8c64dc2c35f
pettepuu <113012213+pettepuu@users.noreply.github.com>
2024-09-10 11:34:45 +0300

    Add files via upload

--- models/person.js
+++ models/person.js
@@ -1,29 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then(() => {
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.error('error connecting to MongoDB:', error.message);
+  });
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minlength: 3
+  },
+    number: {
+      type: String,
+      minlength: 8,
+      validate: function(v) {
+        return /^(\d{2}-\d{6,})|(\d{3}-\d{5,})$/.test(v)
+      }}
 })
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
 module.exports = mongoose.model('Person', personSchema);
